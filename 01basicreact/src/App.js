3112bcf319bd30934186f401b2c97ac8f26fc15a
shyamsoni550 <vayashyam550@gmail.com>
2024-02-21 03:15:13 +0530

    "baics react app"

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>shyams coding journey</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>shyam 's coding jorney with vite  </h1>
+  )
+}
+
+export default App
